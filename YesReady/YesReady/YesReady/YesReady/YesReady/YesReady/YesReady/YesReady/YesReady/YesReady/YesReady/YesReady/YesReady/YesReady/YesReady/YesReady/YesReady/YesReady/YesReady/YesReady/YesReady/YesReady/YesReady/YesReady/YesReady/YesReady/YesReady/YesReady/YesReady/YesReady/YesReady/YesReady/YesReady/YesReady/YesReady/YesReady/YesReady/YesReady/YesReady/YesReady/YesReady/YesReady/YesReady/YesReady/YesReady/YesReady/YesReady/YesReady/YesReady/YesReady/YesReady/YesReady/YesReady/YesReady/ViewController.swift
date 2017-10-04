5068ad6e6a804348cc3520b6f31756cc8ce04f6d
xin <xinliu253@yahoo.com>
2017-10-04 11:35:35 -0400

    adding constraints

---
+++ YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/YesReady/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YesReady
+//
+//  Created by Manzala Kazmi on 9/25/17.
+//  Copyright © 2017 HealthCare. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YesReady/image.swift
@@ -0,0 +1,9 @@
+//
+//  image.swift
+//  YesReady
+//
+//  Created by xin liu on 10/2/17.
+//  Copyright © 2017 HealthCare. All rights reserved.
+//
+
+import Foundation
