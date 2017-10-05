36ceb440bd4f7769ec9125774c08abcf19696316
manzala <mnzl258@gmail.com>
2017-10-05 18:01:50 -0400

    finished signup page

--- YesReady/SignupViewController.swift
+++ YesReady/SignupViewController.swift
@@ -7,17 +7,50 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignupViewController: UIViewController {
 
-    
     @IBOutlet weak var passwordField: UITextField!
     
     @IBOutlet weak var emailField: UITextField!
     
+   
     
     @IBAction func signupButton(_ sender: Any) {
+       
+        if emailField.text == "" || emailField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+        else {
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                 /*   let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                */
+ 
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
