ca964e8417fda9a47bdf6c84db186cb8470ae5e9
manzala <mnzl258@gmail.com>
2017-10-09 02:53:17 -0400

    Fixed up profile page frontend

--- YesReady/ProfileViewController.swift
+++ YesReady/ProfileViewController.swift
@@ -38,9 +38,10 @@ class ProfileViewController: UIViewController {
        override func viewDidLoad() {
         super.viewDidLoad()
         
-        genderField.isOptionalDropDown = false
+        genderField.isOptionalDropDown = true
         genderField.itemList = ["Male", "Female"]
         ageField.itemList = ["16","17","19","20","21","22","23","24","25","26","27","28","29","30"]
+        ageField.isOptionalDropDown = true
 
     }
 
